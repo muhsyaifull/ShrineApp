3844ec90ec9fb67d6441af58310f2419652879c7
MUHAMMAD SYAIFULLAH <mahkotonagata@gmail.com>
2024-02-24 18:43:37 +0700

    First Upload

---
+++ lib/colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+const kShrinePink50 = Color.fromARGB(255, 171, 244, 77);
+const kShrinePink100 = Color.fromRGBO(252, 252, 148, 1);
+const kShrinePink300 = Color.fromARGB(255, 95, 96, 33);
+const kShrinePink400 = Color.fromARGB(255, 68, 125, 45);
+
+const kShrineBrown900 = Color(0xFF442B2D);
+
+const kShrineErrorRed = Color(0xFFC5032B);
+
+const kShrineSurfaceWhite = Color(0xFFFFFBFA);
+const kShrineBackgroundWhite = Colors.white;
+const kShrinePurple = Color(0xFF5D1049);
